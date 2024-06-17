ec025392fc56c8b86e7e08422797304b933c27ae
Pawan <pawankumarpk3610@gmail.com>
2024-06-17 16:55:23 +0530

    Random_Guess_Game

---
+++ script.js
@@ -0,0 +1,37 @@
+/*author - Pawan Kumar */
+let tries = 0;
+let randomNum = Math.floor(Math.random() * 101)
+
+const handleInput = () => {
+   const userInput = document.getElementById('userInput');
+   let inputValue = userInput.value;
+   inputValue = parseInt(inputValue);
+
+   if (isNaN(inputValue) || inputValue < 0 || inputValue > 100) {
+      alert("Please enter a valid number between 0 and 100.");
+      return;
+   }
+
+   tries++;
+
+   let message;
+      if (inputValue > randomNum) {
+         message = "Your guess is greater than random number. Please try again !!"
+      }
+      else if (inputValue < randomNum) {
+         message = "Your guess is smaller than random number. Please try again !!"
+      }
+      else {
+         const score = 101 - tries;
+         message = "Congratulations !! You guessed the correct Number.\n Your Score is :" + score;
+
+         tries = 0;
+         randomNum = Math.floor(Math.random() * 101);
+      }
+      
+
+   const outputElement = document.getElementById('output');
+   outputElement.textContent = message;
+
+   userInput.value = '';
+}
